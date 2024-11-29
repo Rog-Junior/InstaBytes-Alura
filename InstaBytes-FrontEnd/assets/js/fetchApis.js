ae6d53cf09b17a096a55597a98e9fb4c3f426657
Rog Junior <rogerio.dev@outlook.com.br>
2024-11-29 15:03:44 -0300

    Imersão Alura - InstaBytes

---
+++ InstaBytes-FrontEnd/assets/js/fetchApis.js
@@ -0,0 +1,10 @@
+// Função para buscar os dados do endpoint
+export default async function fetchImages() {
+  try {
+    const response = await fetch(process.env.API_URL); // Usando a URL importada
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error);
+  }
+}
